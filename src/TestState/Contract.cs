aeb4d36d7bf22f5f6d42d9c51627d678feedc881
Behzad <behzad.khosravifar@gmail.com>
2022-05-23 11:54:40 +0430

    Reduce contract entity size

--- src/TestState/Contract.cs
+++ src/TestState/Contract.cs
@@ -7,16 +7,11 @@
         public int BuyerUserId { get; set; }
         public int? BuyerAddressId { get; set; }
         public string Descriptions { get; set; }
-        public int? TransportPrice { get; set; }
-        public int? AddedContractPrice { get; set; }
         public int? ContractPrice { get; set; }
         public int TotalPrice { get; set; }
-        public double? Stars { get; set; }
         public ContractStatus Status { get; set; }
+        public bool IsCashPaymentEnabled { get; set; }
         public DateTime? EndTime { get; set; }
         public DateTime? StartTime { get; set; }
-        public bool IsCashPaymentEnabled { get; set; }
-
-        public int TotalHours => (int)Math.Ceiling((EndTime - StartTime).Value.TotalHours);
     }
 }
